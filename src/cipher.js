796232a37beaa62e4174c313e16048ec4e95b4c0
JanetGM <antaly.146@gmail.com>
2018-11-20 13:05:13 -0500

    ya cifra palabra ingresando en caja de texto

--- src/cipher.js
+++ src/cipher.js
@@ -1,12 +1,4 @@
 window.cipher = {
   // ...
-  const word=prompt("Ingresa tu palabra");
-  console.log(word.charCodeAt(0));
-  const newword=(word.charCodeAt(0))+1;
-  console.log(newword);
-  const newletter=String.fromCharCode(newword);
-  console.log(newletter);
-
-
 
 };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 
+
+const btnCipher=document.getElementById('btn-cipher');
+const message=document.getElementById("message");
+const moveTo=document.getElementById("moveTo");
+
+
+function manejadorClick(){
+  console.log(typeof message.value)
+  console.log(typeof parseInt(moveTo.value))
+  Cifrar(message.value,parseInt( moveTo.value))
+}
+
+btnCipher.addEventListener('click', manejadorClick)
+
+// btnCipher.addEventListener('click', function(){
+//   console.log(message.value)
+//   console.log(moveTo.value)
+//   Cifrar(message.value,moveTo.value);
+//
+//
 function Cifrar(word,des)
 {
   let newMessage="";
   for (let i = 0; i < word.length; i++) {
       let numberAscii=((word[i].charCodeAt())-65+des)%26+65;
-      alert(numberAscii);
+
       let lettercipher=String.fromCharCode(numberAscii);
-      alert(lettercipher);
+
       newMessage=newMessage+lettercipher;
-      alert(newMessage);
+
   }
+    alert(newMessage);
 }
-Cifrar('aaa',1);
