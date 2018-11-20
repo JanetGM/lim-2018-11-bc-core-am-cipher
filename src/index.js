54b8b3fcb97da0145e7884d4a741a8dec6f2a242
JanetGM <antaly.146@gmail.com>
2018-11-20 10:33:13 -0500

    ya cifra una palabra

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("sdasd");
+
+function Cifrar(word,des)
+{
+  let newMessage="";
+  for (let i = 0; i < word.length; i++) {
+      let numberAscii=((word[i].charCodeAt())-65+des)%26+65;
+      alert(numberAscii);
+      let lettercipher=String.fromCharCode(numberAscii);
+      alert(lettercipher);
+      newMessage=newMessage+lettercipher;
+      alert(newMessage);
+  }
+}
+Cifrar('aaa',1);
