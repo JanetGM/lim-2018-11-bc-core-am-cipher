0f3566d6b269e73bc8970514d3c2d7c1c82d4ad0
JanetGM <antaly.146@gmail.com>
2018-11-26 16:31:15 -0500

    cambios de la portada style.css

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ const btnCipher=document.getElementById('btn-cipher');//CIFRAR
 const btnDecipher=document.getElementById('btn-decipher');//DECIFRAR
 const message=document.getElementById("message");//MENSAJE A CIFRAR
 const moveTo=document.getElementById("moveTo");//DESPLAMIENTO
-const messageHide=document.getElementById("message-hide");//MENSAJE PARA decifrar
-const moveToBack=document.getElementById("moveTo-back");//DESPLAZAMIENTO
 const textResult=document.getElementById("text-result");//RESULTADOS
 
-
+function ocultar()
+{
+  
+}
 
 function manejadorClick(){
   textResult.value=cipher.encode(message.value,parseInt( moveTo.value));
