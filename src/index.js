2068c129a47d97c79f65158392d12f0a71891669
JanetGM <antaly.146@gmail.com>
2018-12-03 21:16:44 -0500

    cambios ultimos de html , y js

--- src/index.js
+++ src/index.js
@@ -6,26 +6,35 @@ const message=document.getElementById("message");//MENSAJE A CIFRAR
 const moveTo=document.getElementById("moveTo");//DESPLAMIENTO
 const textResult=document.getElementById("text-result");//RESULTADOS
 const buttonStart=document.getElementById("start");//boton de empezar
-
+const buttonClean=document.getElementById("btn-clean");
 
 function manejadorClick(){
-  textResult.value=cipher.encode(parseInt(moveTo.value),message.value);
+  textResult.value=cipher.encode(parseInt( moveTo.value),message.value);
 }
 function manejadorClick2() {
-  textResult.value=cipher.decode(parseInt(moveTo.value),message.value);
+  textResult.value=cipher.decode(parseInt( moveTo.value),message.value);
 }
 
 function manejadorClick3(){
   showCipherView()
 }
+function manejadorClick4() {
+  clean()
+}
 function showCipherView()
 {
-  document.getElementById('instruction').style.display="none";
-  document.getElementById('titulo-content').innerHTML="Bottle Up";
-  document.getElementById('contenidoDos').style.display="block";
+  document.getElementById("instruction").style.display="none";
+  document.getElementById("titulo-content").innerHTML="  <h1> Bottle Up  </h1>";
+  document.getElementById("contenidoDos").style.display="block";
+}
+function clean() {
+  document.getElementById("message").value="";
+  document.getElementById("moveTo").value="";
+  document.getElementById("text-result").value="";
 }
 
 
 btnCipher.addEventListener('click', manejadorClick);
 btnDecipher.addEventListener('click', manejadorClick2);
 buttonStart.addEventListener('click',manejadorClick3);
+buttonClean.addEventListener('click',manejadorClick4);
